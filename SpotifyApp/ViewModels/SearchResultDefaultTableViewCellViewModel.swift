f3d610127b234bcd65fe5cc5c734fd9c2197eeeb
Desxxxx <desmond.9602@gmail.com>
2022-02-15 16:23:25 +0800

    feat: Implemented Search Result Item Detail List. Mapping All the API Response Data to TableView.

---
+++ SpotifyApp/ViewModels/SearchResultDefaultTableViewCellViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  SearchResultDefaultTableViewCellViewModel.swift
+//  SpotifyApp
+//
+//  Created by Desmond Wong on 15/02/2022.
+//
+
+import Foundation
+
+struct SearchResultDefaultTableViewCellViewModel {
+    let title: String
+    let imageURL: URL?
+}
---
+++ SpotifyApp/ViewModels/SearchResultSubtitleTableViewCellViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  SearchResultSubtitleTableViewCellViewModel.swift
+//  SpotifyApp
+//
+//  Created by Desmond Wong on 15/02/2022.
+//
+
+import Foundation
+
+struct SearchResultSubtitleTableViewCellViewModel {
+    let title: String
+    let subtitle: String
+    let imageURL: URL?
+}
